f6797937bbc4fa987e28447aa6002f6da493ee60
karora1998 <karora1_be16@thapar.edu>
2020-04-19 12:55:41 +0530

    passed data to recipe-item component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Recipe } from '../../recipe.model'
 
 @Component({
   selector: 'app-recipe-item',
@@ -12,4 +14,5 @@ export class RecipeItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @Input() recipe: Recipe;
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,7 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+  new Recipe('A test recipe', 'Simple a test recipe', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'),
   new Recipe('A test recipe', 'Simple a test recipe', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg')
   ];
 
