70bd1ce7b4cde664bb74087795312d3f45f267fa
karora1998 <karora1_be16@thapar.edu>
2020-04-19 12:29:27 +0530

    toggled recipe and shopping-list visibility

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
 	selector: 'app-header',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent{
+
+	@Output() clicked = new EventEmitter<{recipeVisible: boolean, shoppingVisible: boolean}>();
+
+	onRecipeClicked(){
+		this.clicked.emit({ recipeVisible:true, shoppingVisible:false });
+	}
+
+	onShoppingClicked(){
+		this.clicked.emit({ recipeVisible:false, shoppingVisible:true });
+	}
 
 }
