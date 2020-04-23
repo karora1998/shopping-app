07f2ddab83aae0d3450ea0c8c59d1f7cf254dd3b
karora1998 <karora1_be16@thapar.edu>
2020-04-23 13:26:41 +0530

    made 'new recipe' and 'edit recipe' buttons functional

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -14,10 +15,14 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
  
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+  	this.router.navigate(['new'], {relativeTo: this.route});
   }
 
 }
