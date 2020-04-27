2a400462750ace96f2a622aac618feb9f661ad39
karora1998 <karora1_be16@thapar.edu>
2020-04-27 12:29:45 +0530

    made the shopping-edit form template driven and added subject to each item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -8,17 +10,34 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static:false }) nameInput: ElementRef;
-  @ViewChild('amountInput', { static:false }) amountInput: ElementRef; 
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  // @ViewChild('nameInput', { static:false }) nameInput: ElementRef;
+  // @ViewChild('amountInput', { static:false }) amountInput: ElementRef; 
+  @ViewChild('f', {static: true}) form: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.subscription = this.shoppingListService.startedEditing.subscribe(
+      (index: number) => {
+        this.editMode = true;
+        this.editedItemIndex = index;
+      }
+    );
   }
 
-  onAddIngredient() {
-  	this.shoppingListService.onIngredientAdded(new Ingredient(this.nameInput.nativeElement.value, this.amountInput.nativeElement.value));
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
+  onAddItem(form: NgForm) {
+    // const i = new Ingredient(this.nameInput.nativeElement.value, this.amountInput.nativeElement.value);
+  	// this.shoppingListService.onIngredientAdded(i);
+    const value = form.value;
+    const i = new Ingredient(value.name, value.amount);
+    this.shoppingListService.onIngredientAdded(i);
+  }
 }
