4a7419a6ffabd6d6948d61f053d06731861dceb8
karora1998 <karora1_be16@thapar.edu>
2020-04-28 22:47:30 +0530

    made save and fetch options in the header functional using the data-storage service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
+import { DataStorageService } from '../shared/data-storage.service';
+
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html'
@@ -16,5 +18,13 @@ export class HeaderComponent{
 	// onShoppingClicked(){
 	// 	this.clicked.emit({ recipeVisible:false, shoppingVisible:true });
 	// }
+	constructor(private dataStorageService: DataStorageService) {}
 
+	onSaveData() {
+		this.dataStorageService.storedRecipes();
+	}
+
+	onFetchData() {
+		this.dataStorageService.fetchRecipes();
+	}
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-	constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+	storedRecipes() {
+		const recipes = this.recipeService.getRecipes();
+		this.http.put('https://shopping-list-672fc.firebaseio.com/recipes.json', recipes)
+		.subscribe(response => {
+			console.log(response);
+		});
+	}
+
+	fetchRecipes() {
+		this.http.get<Recipe[]>('https://shopping-list-672fc.firebaseio.com/recipes.json')
+		.subscribe(recipes => {
+			console.log(recipes);
+			this.recipeService.setRecipes(recipes);
+		});
+	}
 }
