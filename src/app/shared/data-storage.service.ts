3f39822c77fb39acd7d0193b43a928788077e90d
karora1998 <karora1_be16@thapar.edu>
2020-04-29 14:04:28 +0530

    added auth key to the get request to enable fetching data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-	constructor(private http: HttpClient, private recipeService: RecipeService) {}
+	constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
 	storedRecipes() {
 		const recipes = this.recipeService.getRecipes();
@@ -18,8 +19,11 @@ export class DataStorageService {
 	}
 
 	fetchRecipes() {
-		return this.http.get<Recipe[]>('https://shopping-list-672fc.firebaseio.com/recipes.json')
-		.pipe(map(recipes => {
+		return this.authService.user.pipe(take(1), exhaustMap(user => {
+			return this.http.get<Recipe[]>('https://shopping-list-672fc.firebaseio.com/recipes.json', {
+				params: new HttpParams().set('auth', user.token)
+			})
+		}), map(recipes => {
 			return recipes.map(recipe => {
 				return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
 			})
