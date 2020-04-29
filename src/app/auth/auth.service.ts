cb3c072e430a05e3fd0150e13f902d7273f31098
karora1998 <karora1_be16@thapar.edu>
2020-04-29 12:53:43 +0530

    handled authentication based on firebase parameters

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+
+import { User } from './user.model';
 
 export interface AuthResponseData {
 	kind: string,
@@ -15,6 +17,8 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+	user = new Subject<User>();
+
 	constructor(private http: HttpClient) {}
 
 	signUp(email: string, password: string) {
@@ -24,6 +28,8 @@ export class AuthService {
 			returnSecureToken: true
 		}).pipe(catchError(errorRes => {
 			return this.handleError(errorRes);
+		}), tap(resData => {
+			this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
 		}));
 	}
 
@@ -32,7 +38,15 @@ export class AuthService {
 			email: email, 
 			password: password,
 			returnSecureToken: true
-		}).pipe(catchError(this.handleError));
+		}).pipe(catchError(this.handleError), tap(resData => {
+			this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+		}));
+	}
+
+	private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+		const expirationDate: Date = new Date(new Date().getTime() + (expiresIn)*1000 )
+		const user = new User(email, userId, token, expirationDate);
+		this.user.next(user);
 	}
 
 	private handleError(errorRes: HttpErrorResponse) {
