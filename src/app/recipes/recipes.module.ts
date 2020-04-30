6bbb433721ad96090806cd8a434b207d363df467
karora1998 <karora1_be16@thapar.edu>
2020-04-30 14:14:08 +0530

    created the recipes-routing module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -8,6 +8,7 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
 
 
 @NgModule({
@@ -22,15 +23,8 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 	imports: [
 		RouterModule,
 		CommonModule,
-		ReactiveFormsModule
-	],
-
-	exports: [
-    	RecipesComponent,
-   		RecipeListComponent,
-    	RecipeDetailComponent,
-    	RecipeStartComponent,
-    	RecipeEditComponent
+		ReactiveFormsModule,
+		RecipesRoutingModule
 	]
 })
 export class RecipesModule {}
