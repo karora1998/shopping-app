efde93e01e1d2f183ee150e08821f6de47fa5d6d
karora1998 <karora1_be16@thapar.edu>
2020-04-30 14:25:21 +0530

    created the shopping-list module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { RecipesComponent } from './recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
@@ -15,6 +16,7 @@ import { RecipesRoutingModule } from './recipes-routing.module';
 	declarations: [
 		RecipesComponent,
    		RecipeListComponent,
+    	RecipeItemComponent,
     	RecipeDetailComponent,
     	RecipeStartComponent,
     	RecipeEditComponent
